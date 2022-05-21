96bd41f1cb26f022f722405e29724e90410bb514
VysotskiVadim <vadzim.vysotski@mapbox.com>
2022-05-21 23:00:41 +0200

    combine best

---
+++ app/src/test/java/dev/vadzimv/slowtests/RobolectricPlusMocking.kt
@@ -0,0 +1,83 @@
+package dev.vadzimv.slowtests
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkStatic
+import io.mockk.unmockkStatic
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class RobolectricPlusMocking {
+
+    @Before
+    fun setup() {
+        val mock = mockk<ExampleSmall>()
+        mockkStatic(::plus)
+        every { plus(2, 2) } returns 4
+    }
+
+    @After
+    fun tearDown() {
+        unmockkStatic(::plus)
+    }
+
+    @Test
+    fun `two + two`() {
+        Assert.assertEquals(4, plus(2, 2))
+    }
+
+    @Test
+    fun `two + two copy 1`() {
+        Assert.assertEquals(4, plus(2, 2))
+    }
+
+    @Test
+    fun `two + two copy 2`() {
+        Assert.assertEquals(4, plus(2, 2))
+    }
+
+    @Test
+    fun `two + two copy 3`() {
+        Assert.assertEquals(4, plus(2, 2))
+    }
+
+    @Test
+    fun `two + two copy 4`() {
+        Assert.assertEquals(4, plus(2, 2))
+    }
+
+    @Test
+    fun `two + two copy 5`() {
+        Assert.assertEquals(4, plus(2, 2))
+    }
+
+    @Test
+    fun `two + two copy 6`() {
+        Assert.assertEquals(4, plus(2, 2))
+    }
+
+    @Test
+    fun `two + two copy 7`() {
+        Assert.assertEquals(4, plus(2, 2))
+    }
+
+    @Test
+    fun `two + two copy 8`() {
+        Assert.assertEquals(4, plus(2, 2))
+    }
+
+    @Test
+    fun `two + two copy 9`() {
+        Assert.assertEquals(4, plus(2, 2))
+    }
+
+    @Test
+    fun `two + two copy 10`() {
+        Assert.assertEquals(4, plus(2, 2))
+    }
+}
