a0e972b74d6bbb11841a209c959908dd77f00d8c
VysotskiVadim <vadzim.vysotski@mapbox.com>
2022-05-23 09:24:02 +0200

    mock a few objects

--- app/src/test/java/dev/vadzimv/slowtests/ObjectMockingMockito.kt
+++ app/src/test/java/dev/vadzimv/slowtests/ObjectMockingMockito.kt
@@ -1,23 +1,44 @@
 package dev.vadzimv.slowtests
 
 import org.junit.Assert.assertEquals
+import org.junit.FixMethodOrder
 import org.junit.Test
+import org.junit.runners.MethodSorters
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 class ObjectMockingMockito {
-
-    val plus = mock<Plus> {
-        on { doPlus(2, 2) } doReturn  4
-    }
 
     @Test
     fun `two plus two`() {
+        val plus = createMockPlus()
         assertEquals(4, plus.doPlus(2, 2))
     }
 
     @Test
     fun `two plus two copy 1`() {
+        val plus = createMockPlus()
         assertEquals(4, plus.doPlus(2, 2))
+    }
+
+    @Test
+    fun `two minus two`() {
+        val minus = createMockMinus()
+        assertEquals(0, minus.doMinus(2, 2))
+    }
+
+    @Test
+    fun `two minus two copy 1`() {
+        val minus = createMockMinus()
+        assertEquals(0, minus.doMinus(2, 2))
+    }
+
+    private fun createMockPlus() = mock<Plus> {
+        on { doPlus(2, 2) } doReturn  4
+    }
+
+    private fun createMockMinus() = mock<Minus> {
+        on { doMinus(2, 2) } doReturn 0
     }
 }
