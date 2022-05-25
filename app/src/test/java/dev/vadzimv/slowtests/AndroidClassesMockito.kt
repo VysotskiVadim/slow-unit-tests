37c88fdb3608f099c03d8381470b318dba0edb73
VysotskiVadim <vadzim.vysotski@mapbox.com>
2022-05-25 09:32:58 +0200

    mock different activities

--- app/src/test/java/dev/vadzimv/slowtests/AndroidClassesMockito.kt
+++ app/src/test/java/dev/vadzimv/slowtests/AndroidClassesMockito.kt
@@ -3,37 +3,49 @@ package dev.vadzimv.slowtests
 import android.app.Activity
 import android.content.Context
 import android.location.Location
+import org.junit.FixMethodOrder
 import org.junit.Test
+import org.junit.runners.MethodSorters
 import org.mockito.kotlin.mock
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 class AndroidClassesMockito {
+
     @Test
-    fun `create activity`() {
-        val activity = mock<Activity>()
+    fun `a - warm up mockito`() {
+        mock<Any>()
+    }
+
+    open class Activity1 : Activity()
+    open class Activity2 : Activity()
+
+    @Test
+    fun `b - create activity 1`() {
+        val activity = mock<Activity1>()
     }
 
     @Test
-    fun `create activity 2`() {
-        val activity = mock<Activity>()
+    fun `c - create activity 2`() {
+        val activity = mock<Activity2>()
     }
 
     @Test
-    fun `create context`() {
+    fun `d - create context`() {
         val context = mock<Context>()
     }
 
     @Test
-    fun `create context 2`() {
+    fun `e - create context 2`() {
         val context = mock<Context>()
     }
 
     @Test
-    fun `create location`() {
+    fun `f - create location`() {
         val location = mock<Location>()
     }
 
     @Test
-    fun `create location 2`() {
+    fun `g - create location 2`() {
         val location = mock<Location>()
     }
 }
